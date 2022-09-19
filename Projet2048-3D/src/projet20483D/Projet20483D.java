2f3586e98f7596c8f5ba33ad20de36525730cca1
Questiero <19290903+Questiero@users.noreply.github.com>
2022-09-19 21:59:26 +0200

    Récupération TD 2048
    J'sais pas pourquoi le premier commit il y avait pas totu

--- Projet2048-3D/src/projet20483D/Projet20483D.java
+++ Projet2048-3D/src/projet20483D/Projet20483D.java
@@ -1,20 +1,58 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package projet2048.pkg3d;
+package projet20483D;
 
-/**
- *
- * @author jules
- */
-public class Projet20483D {
+import java.util.Scanner;
+
+public class Projet20483D implements Parametres {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Grille g = new Grille();
+        boolean b = g.nouvelleCase();
+
+        Scanner sc = new Scanner(System.in);
+
+        while (!g.partieFinie()) {
+            
+            System.out.println("Déplacer vers la Droite (d), Gauche (g), Haut (h), ou Bas (b) ?");
+            
+            String s = sc.nextLine();
+            s.toLowerCase();
+            
+            if (!(s.equals("d") || s.equals("droite")
+                    || s.equals("g") || s.equals("gauche")
+                    || s.equals("h") || s.equals("haut")
+                    || s.equals("b") || s.equals("bas"))) {
+                System.out.println("Vous devez écrire d pour Droite, g pour Gauche, h pour Haut ou b pour Bas");
+            } else {
+                int direction;
+                if (s.equals("d") || s.equals("droite")) {
+                    direction = DROITE;
+                } else if (s.equals("g") || s.equals("gauche")) {
+                    direction = GAUCHE;
+                } else if (s.equals("h") || s.equals("haut")) {
+                    direction = HAUT;
+                } else {
+                    direction = BAS;
+                }
+                boolean b2 = g.lanceurDeplacerCases(direction);
+                if (b2) {
+                    b = g.nouvelleCase();
+                    if (!b) {
+                        g.gameOver();
+                    }
+                }
+                System.out.println(g);
+                if (g.getValeurMax() >= OBJECTIF) {
+                    g.victory();
+                }
+            }
+        }
+        
+        g.gameOver();
+
     }
-    
 }
+
