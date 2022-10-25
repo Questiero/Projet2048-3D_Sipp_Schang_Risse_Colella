b6139835de529a5870966c3eea49fac3c1e5233b
Chlonii <93654796+Chlonii@users.noreply.github.com>
2022-10-25 17:58:40 +0200

    premier test fonctionnel FXML

--- Projet2048-3D/src/projet20483D/FXMLController.java
+++ Projet2048-3D/src/projet20483D/FXMLController.java
@@ -6,7 +6,11 @@ package projet20483D;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
 
 /**
  * FXML Controller class
@@ -18,9 +22,16 @@ public class FXMLController implements Initializable {
     /**
      * Initializes the controller class.
      */
+    @FXML
+    private Button test;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
     
+    @FXML
+    private void cliquerBouton(MouseEvent event){
+        System.out.println("ça fonctionne");
+    }
 }
