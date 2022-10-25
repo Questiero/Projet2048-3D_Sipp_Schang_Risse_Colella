82aed71736d529707e72ff3023ad671da7c69c92
Chlonii <93654796+Chlonii@users.noreply.github.com>
2022-10-25 17:36:50 +0200

    Revert "test bouton FXML"
    This reverts commit abc8c3eaf34851c6cc7a1a54531760ec99fbb3e1.

--- Projet2048-3D/src/projet20483D/FXMLController.java
+++ Projet2048-3D/src/projet20483D/FXMLController.java
@@ -4,12 +4,9 @@
  */
 package projet20483D;
 
-import java.awt.event.MouseEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 
 /**
  * FXML Controller class
@@ -21,18 +18,9 @@ public class FXMLController implements Initializable {
     /**
      * Initializes the controller class.
      */
-    
-    @FXML
-    private Button test;
-    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    } 
-    
-    @FXML
-    private void cliquer(MouseEvent event){
-        System.out.println("ça marche");
-    }
+    }    
     
 }
