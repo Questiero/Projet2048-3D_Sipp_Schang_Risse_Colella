1fa4db2fb73f34a6370d41f3b9648cd91587909c
Amandine <91495212+6prine@users.noreply.github.com>
2022-11-01 18:17:24 +0100

    ajouts interface graphique de jeu
    pageJeu.fxml
    les 6 boutons + bouton aléatoire

--- Projet2048-3D/src/projet20483D/PageJeuController.java
+++ Projet2048-3D/src/projet20483D/PageJeuController.java
@@ -6,32 +6,21 @@ package projet20483D;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.input.MouseEvent;
-
 
 /**
  * FXML Controller class
  *
- * @author risse
+ * @author Amandine S
  */
-public class FXMLController implements Initializable {
+public class PageJeuController implements Initializable {
 
     /**
      * Initializes the controller class.
      */
-    @FXML
-    private Button test;
-    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
     
-    @FXML
-    private void cliquerBouton(MouseEvent event){
-        System.out.println("ça fonctionne");
-    }
 }
