c64d8e640df3b7f0abfc0e53f16cea928a18d1df
Amandine <91495212+6prine@users.noreply.github.com>
2022-12-07 16:50:10 +0100

    début database

---
+++ Projet2048-3D/src/projet20483D/database/ParamBDD.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package projet20483D.database;
+
+
+public interface ParamBDD {
+    public static String HOST = "mysql-projet20483d.alwaysdata.net";
+    public static String DBNAME = "projet20483d_bdd";
+    public static String USERNAME = "292158";
+    public static String PWD = "projet20483d_mdp";
+}
---
+++ Projet2048-3D/src/projet20483D/database/Requete.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projet20483D.database;
+
+public class Requete implements ParamBDD {
+
+    private final String host, dbname, username, pwd;
+    private Connexion con = null;
+
+    public Requete() {
+        this.host = HOST;
+        this.dbname = DBNAME;
+        this.username = USERNAME;
+        this.pwd = PWD;
+        this.con = null;
+    }
+
+    public String inscription(String pseudo, String mdp, String mdp2) {
+        try {
+            this.openConnexion();
+        }
+    }
+
+    private void openConnexion() {
+        String connectUrl = "jdbc:mysql://" + this.host + "/" + this.dbname;
+        if (this.con != null) {
+            this.closeConnexion();
+        }
+        try {
+            Class.forName("com.mysql.jdbc.Driver").getDeclaredConstructor().newInstance();
+            this.con = DriverManager.getConnection(connectUrl, this.username, this.pwd);
+        } catch (ClassNotFoundException cnfe) {
+            System.out.println("Cannot load db driver: com.mysql.jdbc.Driver");
+            cnfe.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("Erreur inattendue");
+            e.printStackTrace();
+        }
+    }
+
+    private void closeConnexion() {
+
+    }
+}
